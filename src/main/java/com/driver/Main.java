741d020525cb3f9a0e2da89b0dd682f1ff616d82
denil <deniljohnms27@gmail.com>
2023-01-13 18:07:41 +0530

    add file

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,28 @@ package com.driver;
 
 public class Main {
 
+    Product p = new Product();
+
+    int ansValue = p.product(2,3);
+
+    int secondAnswerValue=p.product(2,3,4);
+
+    double thirdAnswerValue=p.product(2.1,2.9);
+
+}
+class Product{
+    public int product(int x, int y) {
+        int ans=x*y;
+        return ans;
+    }
+    public int product(int x, int y, int z) {
+
+        int secondAnswer=x*y*z;
+        return secondAnswer;
+    }
+
+    public double product(double x, double y) {
+        double thirdAnswer=x*y;
+        return thirdAnswer;
+    }
 }
